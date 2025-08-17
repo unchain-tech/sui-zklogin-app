0519c1e3f63bb08a900a1ed2a04937132ef6d63f
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-08-17 12:13:44 +0900

    Add CI workflow for build and deployment; set base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/sui-zklogin-demo/",
   plugins: [react()],
   esbuild: {
     drop: mode === "production" ? ["console", "debugger"] : [],
