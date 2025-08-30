2ddc2e053da6f07724c15becbdd77a1489711571
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2025-08-30 03:04:21 +0000

    削除

--- src/lib/suiClient.ts
+++
@@ -1,5 +0,0 @@
-import { SuiClient } from "@mysten/sui.js/client";
-import { FULLNODE_URL } from "../utils/constant";
-
-// Sui Clientの初期化（全プロジェクト共通で利用）
-export const suiClient = new SuiClient({ url: FULLNODE_URL });
